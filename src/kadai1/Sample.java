06f5c4d091d68df043c05d6750eab80b44d2fbde
g15956yh <g15956yh@s30407.tsuda.ac.jp>
2018-04-12 13:40:16 +0900

    First commit

---
+++ src/kadai1/Sample.java
@@ -0,0 +1,9 @@
+package kadai1;
+
+public class Sample {
+	public static void main(String[] args) {
+		System.out.println("開発に抵抗がありますが、この授業をきっかけに楽しめるようになりたいです！");
+		System.out.println("よろしくお願いします!");
+	}
+
+}
