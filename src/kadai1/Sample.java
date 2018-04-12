55542ae33442505339c3d4a468570649b7495a04
g15956yh <g15956yh@s30407.tsuda.ac.jp>
2018-04-12 13:54:19 +0900

    second

--- src/kadai1/Sample.java
+++ src/kadai1/Sample.java
@@ -1,9 +1,16 @@
 package kadai1;
 
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
 public class Sample {
 	public static void main(String[] args) {
 		System.out.println("開発に抵抗がありますが、この授業をきっかけに楽しめるようになりたいです！");
 		System.out.println("よろしくお願いします!");
+		
+		Calendar cl = Calendar.getInstance();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+        System.out.println("今日は"+sdf.format(cl.getTime())+"です");
 	}
 
 }
